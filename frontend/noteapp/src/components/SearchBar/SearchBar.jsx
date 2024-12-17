e55550679dc4b5985b577d8f3d1acebad6065270
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2024-12-18 00:19:27 +0630

    searchbar

--- frontend/noteapp/src/components/SearchBar/SearchBar.jsx
+++ frontend/noteapp/src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { FaRegEye } from 'react-icons/fa'
 
-const SearchBar = () => {
+const SearchBar = ({value,onChange,handleSearch,onClearSearch}) => {
   return (
-    <div>SearchBar</div>
+    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
+        <input
+            value={value}
+            onChange={onChange}
+            type="text"
+            placeholder="Search..."
+            className='w-full text-sm py-3 rounded mr-3 outline-none'
+        />
+        <FaRegEye 
+                size={22}
+                onClick={()=>toggleShowPassword()}
+                className="text-primary cursor-pointer"
+        />
+    </div>
   )
 }
 
