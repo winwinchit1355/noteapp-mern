4a94b2ee3f7c841046648c73e672b83a650ac7ab
winwinchit1355 <winwinchit1355@gmail.com>
2024-12-19 16:51:56 +0630

    add modal...

--- frontend/noteapp/src/pages/Home/AddEditNotes.jsx
+++ frontend/noteapp/src/pages/Home/AddEditNotes.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const AddEditNote = () => {
   return (
-    <div>AddEditNote</div>
+    <div>
+      <div className='flex flex-col gap-2'>
+        <label className='input-label'>TITLE</label>
+        <input type="text" className='outline-none text-2xl text-slate-900' placeholder='Go to gym at 5pm' />
+      </div>
+      <div className='flex flex-col gap-2 mt-6'>
+        <label className='input-label'>CONTENT</label>
+        <textarea 
+        type="text"
+        className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
+        placeholder='Content here...'
+        rows={10} />
+      </div>
+      <div>
+        <label className="input-label">TAGS</label>
+      </div>
+      <button className='btn-primary font-medium mt-5 p-3' onClick={() => {}}>ADD</button>
+    </div>
   )
 }
 
--- frontend/noteapp/src/pages/Home/Home.jsx
+++ frontend/noteapp/src/pages/Home/Home.jsx
@@ -1,85 +1,83 @@
-import { IoMdAdd } from "react-icons/io"; 
-import React from 'react'
+import { IoMdAdd } from "react-icons/io";
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../components/Cards/NoteCard'
 import AddEditNote from "./AddEditNotes";
+import Modal from 'react-modal';
 
 const Home = () => {
+  const [openEditModal, setOpenEditModal] = useState({
+    isShown: false,
+    type: "add",
+    date: null,
+  });
   const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   return (
     <>
       <Navbar />
       <div className='grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-2 xl:p-5'>
         <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
-          <NoteCard 
-          title="Internal Meeting for Project A" 
-          date="12/12/2024"
-          content={content}
-          tags="#meeting"
-          isPinned={true}
-          onEdit={() => {}}
-          onDelete={() => {}}
-          onPinNote={() => {}}
+          <NoteCard
+            title="Internal Meeting for Project A"
+            date="12/12/2024"
+            content={content}
+            tags="#meeting"
+            isPinned={true}
+            onEdit={() => { }}
+            onDelete={() => { }}
+            onPinNote={() => { }}
           />
         </div>
         <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
-          <NoteCard 
-          title="Internal Meeting for Project A" 
-          date="12/12/2024"
-          content={content}
-          tags="#meeting"
-          isPinned={true}
-          onEdit={() => {}}
-          onDelete={() => {}}
-          onPinNote={() => {}}
+          <NoteCard
+            title="Internal Meeting for Project A"
+            date="12/12/2024"
+            content={content}
+            tags="#meeting"
+            isPinned={true}
+            onEdit={() => { }}
+            onDelete={() => { }}
+            onPinNote={() => { }}
           />
         </div>
-        <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
-          <NoteCard 
-          title="Internal Meeting for Project A" 
-          date="12/12/2024"
-          content={content}
-          tags="#meeting"
-          isPinned={true}
-          onEdit={() => {}}
-          onDelete={() => {}}
-          onPinNote={() => {}}
-          />
-        </div>
-        <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
-          <NoteCard 
-          title="Internal Meeting for Project A" 
-          date="12/12/2024"
-          content={content}
-          tags="#meeting"
-          isPinned={true}
-          onEdit={() => {}}
-          onDelete={() => {}}
-          onPinNote={() => {}}
-          />
-        </div>
-        <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
-          <NoteCard 
-          title="Internal Meeting for Project A" 
-          date="12/12/2024"
-          content={content}
-          tags="#meeting"
-          isPinned={true}
-          onEdit={() => {}}
-          onDelete={() => {}}
-          onPinNote={() => {}}
-          />
-        </div>
-        <div className="flex items-center justify-center relative bg-slate-200 hover:cursor-pointer border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
-          <button className="" onClick={() => {}}>
+        <div className="flex items-center justify-center relative bg-slate-200 hover:cursor-pointer border rounded-lg shadow-md  transform transition duration-500 hover:scale-105">
+          <button className="" onClick={() => { }}>
             <IoMdAdd size={22} />
           </button>
         </div>
       </div>
-      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-700 absolute right-5 bottom-5" onClick={() => {}}>
-            <IoMdAdd size={30} className="text-white" />
-        </button>
-        <AddEditNote />
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-700 absolute right-5 bottom-5" onClick={() => { 
+        setOpenEditModal({
+          isShown: true,
+          type: "add",
+          date: null,
+        });
+      }}>
+        <IoMdAdd size={30} className="text-white" />
+      </button>
+      
+      <Modal
+        isOpen={openEditModal.isShown}
+        onAfterOpen={()=>{}}
+        onRequestClose={() => {}}
+        style={{overlay:{
+          backgroundColor: 'rgba(0, 0, 0, 0.2)'
+        }}}
+        contentLabel="Example Modal"
+        className=""
+      >
+        <h2>Hello</h2>
+        <button onClick={()=>{}}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+      <AddEditNote />
     </>
   )
 }
