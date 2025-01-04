bf060ed096b4265b0bff93f78ad2a1940ad69eee
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2025-01-04 22:56:30 +0630

    note list

--- frontend/src/components/Cards/NoteCard.jsx
+++ frontend/src/components/Cards/NoteCard.jsx
@@ -1,7 +1,8 @@
 import { MdCreate } from "react-icons/md"; 
 import { MdDelete } from "react-icons/md"; 
 import { AiTwotonePushpin } from "react-icons/ai"; 
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
 const NoteCard = ({title,date,content,tags,isPinned,onEdit,onDelete,onPinNote}) => {
   return (
@@ -9,13 +10,13 @@ const NoteCard = ({title,date,content,tags,isPinned,onEdit,onDelete,onPinNote}) 
         <div className="flex items-center justify-between">
             <div>
                 <h6 className="text-sm font-medium">{title}</h6>
-                <span className="text-xs text-slate-500">{date}</span>
+                <span className="text-xs text-slate-500">{moment(date).format('MMMM Do YYYY')}</span>
             </div>
             <AiTwotonePushpin className={`icon-pin ${isPinned ?'text-primary':'text-slate-300'}`} />
         </div>
         <p className="text-xs text-slate-600 mt-2">{content?.slice(0,60)}</p>
         <div className="flex items-center justify-between mt-2">
-            <div className="text-sm text-slate-500">{tags}</div>
+            <div className="text-sm text-slate-500">{tags.map((item)=> `#${item} `)}</div>
             <div className="flex items-center gap-2">
                 <MdCreate className="icon-btn hover:text-green-600" onClick={onEdit} />
                 <MdDelete className="icon-btn hover:text-red-600" onClick={onDelete} />
